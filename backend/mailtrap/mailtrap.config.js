00dd9b104426c973cb5d0b3f2d258ac1dd1b6ba3
Shagun Sharma <shagu22222@gmail.com>
2024-11-10 00:54:21 +0530

    Initial commit

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,11 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+dotenv.config();
+export const mailtrapClient = new MailtrapClient({
+  token: process.env.TOKENMAILTRAP,
+});
+
+export const sender = {
+  email: "mailtrap@demomailtrap.com",
+  name: "Shagun",
+};
