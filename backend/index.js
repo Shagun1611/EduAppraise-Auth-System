ef815852550f598348ae4d8e4833f86c4cabffd0
Shagun Sharma <119026286+Shagun1611@users.noreply.github.com>
2024-11-10 12:01:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "https://voluble-kringle-b491f4.netlify.app", credentials: true }));
+app.use(cors({ origin: "https://eduappraise.netlify.app", credentials: true }));
 // app.use(cors());
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
